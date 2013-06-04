4e5ca4b9c1ee5fa0d2a4155cb52df50a991486df
ashleygwilliams <heyashleyashley@gmail.com>
2013-06-04 15:45:39 -0400

    add methods for constructing thumbURL and fullURL

--- app/models/placemark.rb
+++ app/models/placemark.rb
@@ -1,3 +1,24 @@
 class Placemark < ActiveRecord::Base
   attr_accessible :latlon, :slug, :year, :year2
+
+  BASE_URL = "http://images.nypl.org/index.php?id="
+
+
+  def getThumbURL
+    imageURL + "&t=b"
+  end
+
+  def getFullURL
+    imageURL + "&t=w"
+  end
+
+  private
+
+  def imageURL
+    if slug[slug.length-1] == 'a'
+      BASE_URL + slug.delete('-a')
+    else
+      BASE_URL + slug.delete('f-')
+    end
+  end
 end
