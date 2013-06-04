f0187ba00e53c7e4ed76dfb0e12385957dec1370
Jeff Larson <thejefflarson@gmail.com>
2013-06-04 13:20:57 -0400

    this -> sky

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NyplData::Application.config.secret_token = '9d888cbf59acdaac7e7a9a2e9ac36c73b8191ef0e8bf11053386d93aaa2228cc411094fb0e4a5540c8ab3fcad908187ec9f34adce2567c32c8277ef5ad8b4165'
