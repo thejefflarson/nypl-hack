ca73d249feefbf4428aef677179b2404a71ba46b
Sisi Wei <me@sisiwei.com>
2013-06-05 10:42:48 -0400

    Rescuing error.

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -10,11 +10,12 @@ class CheckinController < ApplicationController
     url = URI("https://api.foursquare.com/v2/checkins/#{id}/reply")
 
     ours = placemarks_url :q => "#{lat},#{lng}"
-    p Net::HTTP.post_form url, {:url => ours, :CHECKIN_ID => id, :text => "Awesome!"}
+    p Net::HTTP.post_form(url, {:url => ours, :CHECKIN_ID => id, :text => "Awesome!"}) rescue Errno::ECONNRESET
 
     respond_to do |format| 
       format.html { render text: "okay" }
       format.json { render json: json }
     end
+
   end
 end
