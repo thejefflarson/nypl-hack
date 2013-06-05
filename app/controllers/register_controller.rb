2932955bbef0c71fd943bfe0c4f49e8d53df3419
Mike Tigas <mike@tig.as>
2013-06-05 13:33:21 -0400

    manually doing oauth

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,5 +1,3 @@
-require 'oauth2'
-
 #FOURSQ_CLIENT_ID = 'GBCU0L2D1WW0PYLSX4EMZSGJ0ZRV21FKNGUCD3SIXHDKKOZN'
 #FOURSQ_CLIENT_SECRET = 'HGSLEOP322JIMKTKHZZGI243D5YYQL5ED5MY0O5IJNWSTO24'
 #FOURSQ_PUSH_SECRET = 'AEDZX2JPWMVJR0ZREHEUGYGRDZS4ICM0WC0UX2E4IKAIKR0R'
@@ -8,17 +6,8 @@ FOURSQ_CLIENT_ID = 'YWTUTS3OL0HWSULMMURGHIU51QDQ4LS5RLGXJDGV3NENV1JE'
 FOURSQ_CLIENT_SECRET = 'RXXELPAKSE4AXYADZIRHFHC5HIK4OIP5A115H1VHV0OLANCE'
 FOURSQ_PUSH_SECRET = 'XVTHZ2DZHCR2K5PZ3UBVQXBQUABKH3YO3BE5QUF4TEGHKTB5'
 
-
 class RegisterController < ApplicationController
   def create
-    client = OAuth2::Client.new(FOURSQ_CLIENT_ID, FOURSQ_CLIENT_SECRET,
-      :site => 'http://foursquare.com/v2/',
-      :request_token_path => "/oauth2/request_token",
-      :access_token_path  => "/oauth2/access_token",
-      :authorize_path     => "/oauth2/authenticate?response_type=code",
-      :parse_json => true
-    )
-
     redirect_uri = "https://mike.tig.as/nypl-hack/register_callback"
 
     uri = URI.parse("https://foursquare.com/oauth2/access_token?client_id=#{FOURSQ_CLIENT_ID}&client_secret=#{FOURSQ_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=#{redirect_uri}&code=" + params[:code])
@@ -28,15 +17,25 @@ class RegisterController < ApplicationController
 
     request = Net::HTTP::Get.new(uri.request_uri)
     response = JSON.parse(http.request(request).body)
-    access_token = OAuth2::AccessToken.new(client, response["access_token"])
     str_token = response['access_token']
     #puts access_token
 
-    user = access_token.get('https://api.foursquare.com/v2/users/self')
-    puts user.inspect
-    puts user.id
+    self_url = URI("https://api.foursquare.com/v2/users/self")
+    http = Net::HTTP.new(reply_url.host, reply_url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    body_data = URI.encode_www_form(
+      "oauth_token" => str_token,
+      "v" => 20130605
+    )
+    puts body_data
 
-    Accesstoken.create(:access_token => str_token, :user_id => user.id)
+    request = Net::HTTP::Post.new(self_url.request_uri)
+    request['Content-Type'] = "application/x-www-form-urlencoded"
+    response = http.request(request, body_data)
+    puts response.body
+
+    #json = JSON.parse(response.body)
 
     respond_to do |format|
       format.html { render text: "Yay!\n#{str_token}" }
