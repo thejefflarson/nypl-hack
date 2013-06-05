d37ea64601c8beb036c76aac0e632f94462a1317
Mike Tigas <mike@tig.as>
2013-06-05 11:23:42 -0400

    user account hookup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NyplData::Application.routes.draw do
+  match "/register_callback" => "register#create"
   resources :checkin, :only => [:create]
   resources :placemarks, :only => [:index, :show]
 end
