2420467c2c5362060a3f9f25a5fba6ceb162e76c
Sisi Wei <me@sisiwei.com>
2013-06-05 14:30:14 -0400

    Product page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.masonry.min.js
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 NyplData::Application.routes.draw do
   match "/register_callback" => "register#create"
+  match "/" => "checkin#show"
+
   resources :checkin, :only => [:create]
   resources :placemarks, :only => [:index, :show]
 end
