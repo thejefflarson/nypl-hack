71a98ccb43d0ecc6857e5a8fa6d55b9a3983e3f5
Sisi Wei <me@sisiwei.com>
2013-06-05 15:18:01 -0400

    More styles

--- app/assets/javascripts/checkin.js
+++ app/assets/javascripts/checkin.js
@@ -3,10 +3,14 @@
 $(document).ready(function(){
 
 	var $container = $('#masonjar');
-
 	$container.imagesLoaded( function(){
 	  $container.masonry({
 	    itemSelector : '.box'
 	  });
 	});
+
+	$('.connect-button').click(function(){
+		window.location.href = "https://foursquare.com/app/nypl-time-traveller/GBCU0L2D1WW0PYLSX4EMZSGJ0ZRV21FKNGUCD3SIXHDKKOZN";
+	})
+
 })
